7befb832772e59c9f3491df635a3694e3e6894b7
nekon <nobodqwe@gmail.com>
2025-02-21 19:35:13 +0300

    changed structure of container, added iterators

--- RawBase.h
+++ RawBase.h
@@ -10,9 +10,7 @@ private:
 	size_t size = 0;
 	size_t capacity = 1;
 	T* normalize_capacity() {
-		while (capacity <= size) {
-			capacity *= 2;
-		}
+		capacity = std::max(capacity * 2, size + 1); 
 		T* newData = (T*)malloc(sizeof(T) * capacity);
 		if (!newData) {
 			throw std::bad_alloc();
@@ -22,39 +20,59 @@ private:
 		}
 		return newData;
 	}
+
+	T* normalize_capacity_with_initializng() {
+		capacity = std::max(capacity * 2, size + 1);
+		T* newData = (T*)calloc(size, sizeof(T));
+		if (!newData) {
+			throw std::bad_alloc();
+		}
+		return newData;
+	}
 public:
 	RawVector() {
 		if (data)
 			free(data);
 		data = (T*)malloc(sizeof(T));
 	};
-    RawVector(size_t size_) : size(size_), capacity(1), data(nullptr) {
-        try {
-			auto newData = normalize_capacity();
+	RawVector(size_t size_) : size(size_), capacity(1), data(nullptr) {
+		try {
+			auto newData = normalize_capacity_with_initializng();
+			data = newData;
+			newData = normalize_capacity();
 			free(data);
 			data = newData;
-        }
-        catch (std::bad_alloc& e) {
-            std::cerr << e.what() << std::endl;
-            free(data);
-            throw;
-        }
-    };
-    RawVector(const RawVector& other) : size(other.size), capacity(other.capacity) {
-        try {
-            T* newData = (T*)calloc(capacity, sizeof(T));
-            if (!newData) {
-                throw std::bad_alloc();
-            }
-            std::memcpy(newData, other.data, size * sizeof(T));
-            free(data);
-            data = newData;
-        }
-        catch (std::bad_alloc& e) {
-            std::cerr << e.what() << std::endl;
-            throw;
-        }
-    };
+		}
+		catch (std::bad_alloc& e) {
+			std::cerr << e.what() << std::endl;
+			free(data);
+			throw;
+		}
+	};
+	RawVector(const RawVector& other) : size(other.size), capacity(other.capacity) {
+		try {
+			T* newData = (T*)calloc(capacity, sizeof(T));
+			if (!newData) {
+				throw std::bad_alloc();
+			}
+			std::memcpy(newData, other.data, size * sizeof(T));
+			free(data);
+			data = newData;
+		}
+		catch (std::bad_alloc& e) {
+			std::cerr << e.what() << std::endl;
+			throw;
+		}
+	}
+
+	RawVector(RawVector&& other) noexcept
+		: data(other.data), size(other.size), capacity(other.capacity) {
+
+		other.data = nullptr;
+		other.size = 0;
+		other.capacity = 0;
+	}
+
 	RawVector& operator=(const RawVector& other) {
 		if (this == &other) {
 			return *this;
@@ -69,46 +87,36 @@ public:
 			data = newData;
 			size = other.size;
 			capacity = other.capacity;
-			data = normalize_capacity();
 		}
 		catch (std::bad_alloc& e) {
 			std::cerr << e.what() << std::endl;
 			throw;
 		}
 		return *this;
-	};
-	RawVector& operator=(const RawVector&& other) noexcept{
-		if (this == &other) {
-			return *this;
-		}
-		try {
-			T* newData = (T*)calloc(other.capacity, sizeof(T));
-			if (!newData) {
-				throw std::bad_alloc();
-			}
-			std::memcpy(newData, other.data, other.size * sizeof(T));
+	}
+	RawVector& operator=(RawVector&& other) noexcept {
+		if (this != &other) {
 			free(data);
-			data = newData;
+			data = other.data;
 			size = other.size;
 			capacity = other.capacity;
-		}
-		catch (std::bad_alloc& e) {
-			std::cerr << e.what() << std::endl;
-			if (data)
-				free(data);
-			throw;
+			other.data = nullptr;
+			other.size = 0;
+			other.capacity = 0;
 		}
 		return *this;
 	}
 	void push_back(const T& elem) {
 		try {
 			if (capacity <= size) {
-				auto newData = normalize_capacity();
-				std::memcpy(newData, data, size * sizeof(T));
-				if (data != nullptr && newData != data) {
+				capacity *= 2;
+				auto newdata = (T*)std::realloc(data, capacity  * sizeof(T));
+				if (!newdata) {
+					std::cerr << "Couldn't reserve that much space ERR" << std::endl;
 					free(data);
+					throw std::bad_alloc();
 				}
-				data = newData;
+				data = newdata;
 			}
 			data[size] = elem;
 			++size;
@@ -118,41 +126,76 @@ public:
 			throw;
 		}
 	}
-	T& operator [] (const size_t index) {
-		if (index >= size) {
-			throw std::out_of_range("Index out of range");
-		}
-		return data[index];
-	};
 
-	const T& operator [] (const size_t index) const {
-		if (index >= size) {
+	void push_back(T&& elem) {
+		try {
+			if (capacity <= size) {
+				auto newData = normalize_capacity();
+				std::memcpy(newData, data, size * sizeof(T));
+				if (data != nullptr && newData != data) {
+					free(data);
+					data = nullptr;
+				}
+				data = newData;
+			}
+			data[size] = std::move(elem);
+			++size;
+		}
+		catch (...) {
+			std::cerr << "Something happened in push_back" << std::endl;
+			throw;
+		}
+	}
+
+	T& at(const size_t index) {
+		if (index >= size || index < 0) {
 			throw std::out_of_range("Index out of range");
 		}
 		return data[index];
 	}
 
+	const T& at(const size_t index) const {
+		if (index >= size || index < 0) {
+			throw std::out_of_range("Index out of range");
+		}
+		return data[index];
+	}
+
+	T& operator [] (const size_t index) {
+		return data[index];
+	}
+
+	const T& operator [] (const size_t index) const {
+		return data[index];
+	}
+
 	size_t get_size() const {
 		return size;
-	};
+	}
 	size_t get_capacity() const {
 		return capacity;
-	};
+	}
 
 	void resize(size_t new_size) {
+		if (new_size < size) {
+			size = new_size;
+			return;
+		}
 		T* new_data = (T*)realloc(data, new_size * sizeof(T));
 		if (!new_data) {
 			std::cerr << "Couldn't reserve that much space ERR" << std::endl;
 			throw std::bad_alloc();
 		}
 		data = new_data;
-		if (new_size > size)
-			std::memset(data + size, 0, (new_size - size) * sizeof(T));
+		std::cout << data + size << std::endl;
+		if (new_size > size) { std::memset(data + size, 0, (new_size - size) * sizeof(T)); }
 		size = new_size;
+		auto newdata = normalize_capacity();
+		data = newdata;
 	}
 
 	void reserve(size_t reserve_size) {
-		if(reserve_size < capacity){
+		if (reserve_size < capacity) {
 			return;
 		}
 		try {
@@ -170,36 +213,176 @@ public:
 			throw;
 		}
 		catch (...) {
-			std::cerr << "What tf happened here? P.S -> (check reserve function)";
+			std::cerr << "What tf happened here? P.S. -> (check reserve function)";
 			throw;
 		}
 	}
 
-	class Pointer {
+	void clear() {
+		if (data) {
+			free(data);
+			data = nullptr;
+		}
+		size = 0;
+		capacity = 1;
+	}
+
+	void shrink_to_fit() {
+		if (size != capacity && size != 0) {
+			try {
+				auto shrinked = (T*)realloc(data, size * sizeof(T));
+				if (!shrinked) {
+					free(data);
+					data = nullptr;
+					throw std::bad_alloc();
+				}
+				data = shrinked;
+				capacity = size;
+			}
+			catch (std::bad_alloc& err) {
+				std::cerr << err.what() << std::endl;
+				throw;
+			}
+		}
+		if (size == 0) { free(data); data = nullptr; }
+	}
+
+	void pop_back() {
+		if (size == 0) {
+			throw std::out_of_range("Vector is empty");
+		}
+		if (--size == 0) { free(data); data = nullptr; }
+	}
+
+	void insert(size_t index, const T& value) {
+		if (index > size) {
+			throw std::out_of_range("Index out of range");
+		}
+		if (size == capacity) {
+			auto newData = normalize_capacity();
+			std::memcpy(newData, data, size * sizeof(T));
+			free(data);
+			data = newData;
+		}
+		std::memmove(data + index + 1, data + index, (size - index) * sizeof(T));
+		data[index] = value;
+		++size;
+	}
+
+	void insert(size_t index, T&& value) {
+		if (index > size || index < 0) {
+			throw std::out_of_range("Index out of range");
+		}
+		if (size == capacity) {
+			auto newData = normalize_capacity();
+			std::memcpy(newData, data, size * sizeof(T));
+			free(data);
+			data = newData;
+		}
+		std::memmove(data + index + 1, data + index, (size - index) * sizeof(T));
+		data[index] = std::move(value);
+		++size;
+	}
+
+	void erase(size_t index) {
+		if (index >= size || index < 0) {
+			throw std::out_of_range("Index out of range");
+		}
+		std::memmove(data + index, data + index + 1, (size - index - 1) * sizeof(T));
+		if (--size == 0) { free(data); data = nullptr; }
+	}
+
+	void swap(RawVector& other) noexcept {
+		std::swap(data, other.data);
+		std::swap(size, other.size);
+		std::swap(capacity, other.capacity);
+	}
+
+	class Iterator {
 	private:
 		T* ptr;
 	public:
-		Pointer(T* p = nullptr)
-			: ptr(p) {
-		}
+		Iterator(T* ptr_) : ptr(ptr_) {};
 
+		bool operator ==(Iterator& Iteratorr) { return ptr == Iteratorr.ptr; }
+		bool operator !=(Iterator& Iteratorr) { return ptr != Iteratorr.ptr; }
 
+		T& operator *() const { return *ptr; }
+		T* operator ->() const { return ptr; }
 
-		~Pointer() {
-			if (ptr) {
-				free(ptr);
-			}
-		}
+		Iterator& operator ++() { ++ptr; return *this; }
+		Iterator operator ++(int) { Iterator tmp = *this; ++ptr; return tmp; }
+
+		Iterator& operator --() { --ptr; return *this; }
+		Iterator operator --(int) { Iterator tmp = *this; --ptr; return tmp; }
+
+		bool operator==(const Iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const Iterator& other) const { return ptr != other.ptr; }
+		bool operator< (const Iterator& other) const { return ptr <  other.ptr; }
+		bool operator> (const Iterator& other) const { return ptr >  other.ptr; }
+		bool operator<=(const Iterator& other) const { return ptr <= other.ptr; }
+		bool operator>=(const Iterator& other) const { return ptr >= other.ptr; }
+
 	};
+
+	Iterator begin() { return Iterator(data); }
+	Iterator end() { return Iterator(data + size); }
+	Iterator rbegin() { return size > 0 ? Iterator(data + size - 1) : Iterator(data); }
+	Iterator rend() { return Iterator(data - 1); }
+
+	Iterator data_get() { return begin(); }
+
+	T& front() { return *begin(); }
+	T& back() { return *rbegin(); }
+
+
+	class ConstIterator {
+	private:
+		const T* ptr;
+	public:
+		ConstIterator(const T* ptr_) : ptr(ptr_) {}
+
+		bool operator==(const ConstIterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const ConstIterator& other) const { return ptr != other.ptr; }
+
+		const T& operator*() const { return *ptr; }
+		const T* operator->() const { return ptr; }
+
+		ConstIterator& operator++() { ++ptr; return *this; }
+		ConstIterator operator++(int) { ConstIterator tmp = *this; ++ptr; return tmp; }
+
+		ConstIterator& operator--() { --ptr; return *this; }
+		ConstIterator operator--(int) { ConstIterator tmp = *this; --ptr; return tmp; }
+
+		bool operator<(const ConstIterator& other) const { return ptr < other.ptr; }
+		bool operator>(const ConstIterator& other) const { return ptr > other.ptr; }
+		bool operator<=(const ConstIterator& other) const { return ptr <= other.ptr; }
+		bool operator>=(const ConstIterator& other) const { return ptr >= other.ptr; }
+	};
+
+	using const_iterator = ConstIterator;
+
+	const_iterator cbegin() const { return const_iterator(data); }
+	const_iterator cend() const { return const_iterator(data + size); }
+	const_iterator crbegin() const { return size > 0 ? const_iterator(data + size - 1) : const_iterator(data); }
+	const_iterator crend() const { return const_iterator(data - 1); }
+
+	const Iterator data_get() const { return cbegin(); }
+
+	const T& back() const { return *crbegin(); }
+	const T& front() const { return *cbegin(); }
 
 	RawVector copy() {
 		RawVector<T> new_vector(size);
+		new_vector.reserve(capacity);
 		std::memcpy(new_vector.data, data, size * sizeof(T));
 		return new_vector;
 	}
+
+	bool empty() {return size == 0;}
+
 	~RawVector() {
-		if(data)
-			free(data);
+		if (data) { std::cout << "Freeing memory at address: " << static_cast<void*>(data) << " | "; free(data); }
 		std::cout << "Object Destroyed with size: " << size << " and with capacity: " << capacity << std::endl;
 	};
 };
--- tests.cpp
+++ tests.cpp
@@ -1,13 +1,150 @@
-#include <iostream>
-#include <cstdlib> // для malloc и free
-#include <cstring> // для memcpy
+п»ї#include <iostream>
+#include <cassert>
+#include <crtdbg.h>
+#include <chrono>
 #include "RawBase.h"
 
+template<typename Func>
+void measure_time(const char* name, Func func) {
+    auto start = std::chrono::high_resolution_clock::now();
+    func();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << name << ": "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+        << " ms\n";
+}
+
+void test_push_back() {
+    RawVector<int> rv;
+    measure_time("RawVector push_back", [&]() {
+        rv.reserve(134217728);
+        for (int i = 0; i < 100000000; ++i) {
+            rv.push_back(i);
+        }
+        });
+    assert(rv.get_size() == 100000000);
+    std::cout << "Push_back test passed.\n";
+}
+
+void test_resize() {
+    RawVector<int> rv;
+    measure_time("RawVector resize", [&]() {
+        rv.resize(1000000);
+        });
+    assert(rv.get_size() == 1000000);
+    std::cout << "Resize test passed.\n";
+}
+
+void test_reserve() {
+    RawVector<int> rv;
+    measure_time("RawVector reserve", [&]() {
+        rv.reserve(1000000);
+        });
+    assert(rv.get_capacity() >= 1000000);
+    std::cout << "Reserve test passed.\n";
+}
+
+void test_access() {
+    RawVector<int> rv;
+    rv.resize(100);
+    for (int i = 0; i < 100; ++i) {
+        rv[i] = i;
+    }
+    measure_time("RawVector access", [&]() {
+        for (int i = 0; i < 100; ++i) {
+            assert(rv[i] == i);
+        }
+        });
+    std::cout << "Access test passed.\n";
+}
+
+void test_iterator() {
+    RawVector<int> rv;
+    rv.resize(100);
+    for (int i = 0; i < 100; ++i) {
+        rv[i] = i;
+    }
+    measure_time("RawVector iterator", [&]() {
+        int sum = 0;
+        for (auto it = rv.begin(); it != rv.end(); ++it) {
+            sum += *it;
+        }
+        assert(sum == 4950);
+        });
+    std::cout << "Iterator test passed.\n";
+}
+
+void test_copy() {
+    RawVector<int> rv;
+    rv.resize(100);
+    for (int i = 0; i < 100; ++i) {
+        rv[i] = i;
+    }
+    measure_time("RawVector copy", [&]() {
+        RawVector<int> rv_copy = rv.copy();
+        for (int i = 0; i < 100; ++i) {
+            assert(rv_copy[i] == i);
+        }
+        });
+    std::cout << "Copy test passed.\n";
+}
+
+void test_clear() {
+    RawVector<int> rv;
+    rv.resize(100);
+    measure_time("RawVector clear", [&]() {
+        rv.clear();
+        });
+    assert(rv.get_size() == 0);
+    std::cout << "Clear test passed.\n";
+}
+
+void test_erase() {
+    RawVector<int> rv;
+    rv.resize(100);
+    for (int i = 0; i < 100; ++i) {
+        rv[i] = i;
+    }
+    measure_time("RawVector erase", [&]() {
+        rv.erase(50);
+        });
+    assert(rv.get_size() == 99);
+    std::cout << "Erase test passed.\n";
+}
+
+void test_insert() {
+    RawVector<int> rv;
+    rv.resize(100);
+    for (int i = 0; i < 100; ++i) {
+        rv[i] = i;
+    }
+    measure_time("RawVector insert", [&]() {
+        rv.insert(50, 999);
+        });
+    assert(rv[50] == 999);
+    assert(rv.get_size() == 101);
+    std::cout << "Insert test passed.\n";
+}
+
 int main() {
-	RawVector<size_t> v;
-	v.reserve(100);
-	v.resize(10);
-	for (size_t i = 0; i < 10; ++i)
-		std::cout << v[i] << " ";
-	std::cout << std::endl;
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    test_push_back();
+    test_resize();
+    test_reserve();
+    test_access();
+    test_iterator();
+    test_copy();
+    test_clear();
+    test_erase();
+    test_insert();
+    RawVector<int> V(1000);
+    auto start = std::chrono::high_resolution_clock::now();
+    V.resize(10);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "resized to less test (should be near zero)" << ": "
+        << (end - start).count()
+        << " nanos\n";
+    _CrtDumpMemoryLeaks();
+    return 0;
 }
